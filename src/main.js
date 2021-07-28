0745ce16d7f3bb9bfc42154be1983f3c5db1324e
Dmitry Kilin <coding@kilin.biz>
2021-07-28 20:14:03 +0300

    render films lists and popup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import {createUserProfileTemplate} from './view/user-profile.js';
-import {createMainNavigationTemplate} from './view/main-navigation.js';
-import {createSortTemplate} from './view/sort.js';
+import {createMenuTemplate} from './view/menu.js';
+import {createFilmCardTemplate} from './view/film-card.js';
+import {createShowMoreTemplate} from './view/show-more.js';
 import {createFooterStatisticsTemplate} from './view/footer-statistics.js';
+import {createFilmDetailsTemplate} from './view/film-details.js';
+
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -9,9 +12,34 @@ const render = (container, template, place) => {
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const filmsListCatalogElement = document.querySelector('.films-list__container--catalog');
+const filmsListTopRatedElement = document.querySelector('.films-list__container--top-rated');
+const filmsListMostCommentedElement = document.querySelector('.films-list__container--most-commented');
 const siteFooterElement = document.querySelector('.footer');
+const sitePopupElement = document.querySelector('.film-details');
+
+
+const FILMS_LIST_COUNT = 5;
+const EXTRA_FILMS_LIST_COUNT = 2
 
 render(siteHeaderElement, createUserProfileTemplate(), 'beforeend');
-render(siteMainElement, createMainNavigationTemplate(), 'afterbegin');
-render(siteMainElement, createSortTemplate(), 'beforeend');
+render(siteMainElement, createMenuTemplate(), 'afterbegin');
+
+for (let i = 0; i < FILMS_LIST_COUNT; i++) {
+  render(filmsListCatalogElement, createFilmCardTemplate(), 'afterbegin');
+}
+
+render(filmsListCatalogElement, createShowMoreTemplate(), 'afterend');
+
+for (let i = 0; i < EXTRA_FILMS_LIST_COUNT; i++) {
+  render(filmsListTopRatedElement, createFilmCardTemplate(), 'afterbegin');
+}
+for (let i = 0; i < EXTRA_FILMS_LIST_COUNT; i++) {
+  render(filmsListMostCommentedElement, createFilmCardTemplate(), 'afterbegin');
+}
+
 render(siteFooterElement, createFooterStatisticsTemplate(), 'beforeend');
+
+render(sitePopupElement, createFilmDetailsTemplate(), 'afterbegin');
+
+
