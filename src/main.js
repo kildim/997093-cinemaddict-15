d646c1e10680797e3b87a1bb2be7536490d966e9
Dmitry Kilin <coding@kilin.biz>
2021-08-14 22:04:53 +0300

    correct onEscKeyDown

--- src/main.js
+++ src/main.js
@@ -93,7 +93,6 @@ if (siteState.hasState(STATES['all'])) {
       if (evt.key === 'Escape' || evt.key === 'Esc') {
         evt.preventDefault();
         filmDetails.clearContent();
-        document.removeEventListener('keydown', onEscKeyDown);
       }
     };
 
